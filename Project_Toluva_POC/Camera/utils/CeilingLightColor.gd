4d420e6f2051765662f8c4bd3f67e2958c9fc4ae
mmurawski <matimtt@yahoo.com>
2020-06-30 15:30:08 -0700

    changed lights a little

---
+++ Project_Toluva_POC/Camera/utils/CeilingLightColor.gd
@@ -0,0 +1,28 @@
+extends "res://CeilingLight.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var red_color = .65
+var blue_color = .25
+var color1 = Color( red_color, 0.16, blue_color, 1 )
+var time = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta #time elapsed in ms since delta is in ms
+	
+	red_color = .75+(cos(time)/8)
+	blue_color = .25+(sin(time)/8)
+		
+
+	#print("red ", red_color)
+	#print("blue ", blue_color)
+	
+	color1 = Color( red_color, 0.16, blue_color, 1 )
+	light_color = color1
